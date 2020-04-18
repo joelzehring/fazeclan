6a85701a72b46d348789a37672dd6991fe542788
phil cowan <philipjcowan1@gmail.com>
2020-04-18 16:48:03 -0700

    deleting a bunch of cra junk

--- fazeclan/src/App.js
+++ fazeclan/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Layout/Header'
-import ActivUser from './components/Layout/ActiveUser'
+import ActiveUser from './components/Layout/ActiveUser'
 
 function App() {
   return (
     <div className="App">
+      
       <Header />
-      <ActivUser />
+      <ActiveUser />
+      
     </div>
   );
 }
