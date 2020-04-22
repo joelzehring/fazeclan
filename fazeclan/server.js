d4a247410681d189ba6e47fd6bd380121952d7d0
Rachel Sipes <rasipes@gmail.com>
2020-04-21 22:47:48 -0400

    updated folder structure and added beginning graphql api routes

--- fazeclan/server.js
+++ fazeclan/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gitclub");
 
 // Start the API server
 app.listen(PORT, function() {
