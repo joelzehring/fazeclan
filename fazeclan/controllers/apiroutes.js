87a39e562b12ca49a020889da2f9d77cee4245f6
Rachel Sipes <rasipes@gmail.com>
2020-04-25 22:11:51 -0400

    added working code to request data from github graphql

--- fazeclan/controllers/apiroutes.js
+++ fazeclan/controllers/apiroutes.js
@@ -1,42 +1,7 @@
 // const axios = require("../client/node_modules/axios");
 
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
-type Query {
-    {
-        viewer {
-          login
-      
-        }
-        user(login: "sharkrachel") {
-          bio
-          followers {
-            totalCount
-          }
-          name
-          avatarUrl
-          repositories {
-            totalCount
-          }
-              contributionsCollection {
-            totalCommitContributions
-          }
-        }
-      }
-}`);
 
-var root = { hello: () => 'Hello world!' };
-
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
 
 
 
