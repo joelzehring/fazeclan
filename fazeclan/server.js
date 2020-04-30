02352ff007a4671c0cb68020975760798df67546
Joel Zehring <joelzehring@gmail.com>
2020-04-29 23:21:27 -0700

    env variable changes for deploy

--- fazeclan/server.js
+++ fazeclan/server.js
@@ -91,7 +91,6 @@ if (process.env.NODE_ENV === "production") {
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gitclub");
 
-
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
