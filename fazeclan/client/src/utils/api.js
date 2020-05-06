b86e80e7fdc1ba9a5250b04ab7ed60014b47e801
Rachel Sipes <rasipes@gmail.com>
2020-05-05 23:49:49 -0400

    moved api call to auth context

--- fazeclan/client/src/utils/api.js
+++ fazeclan/client/src/utils/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default {
     getGraphQL: function(username) {
         console.log("username: ", username);
-        return axios.get("/api/gitinfo/" + username)
+        return axios.get("http://localhost:3001/api/gitinfo/" + username)
     }
 }
