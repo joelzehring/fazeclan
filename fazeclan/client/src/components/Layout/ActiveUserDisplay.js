4ea709cedd1edbb0ff07bff148cc74688c61037d
Rachel Sipes <rasipes@gmail.com>
2020-05-06 00:25:34 -0400

    working api call code by kimberley

--- fazeclan/client/src/components/Layout/ActiveUserDisplay.js
+++ fazeclan/client/src/components/Layout/ActiveUserDisplay.js
@@ -2,7 +2,7 @@ import React from 'react'
 import FormModal from '../UserForm/FormModal';
 import GitBattleButton from '../UserForm/GitBattleButton'
 import Button from '@material-ui/core/Button';
-
+import { AuthContext } from "../../contexts/AuthContext";
 // Overall Page Style
 const activeUserStyle = {
     color: 'white',
@@ -16,16 +16,13 @@ const activeUserStyle = {
     marginRight: '20px',
     marginBottom: '10px'
 }
-
 const activeUserInfoStyle = {
     textAlign: 'center'
 };
-
 // Buttons Parent Style
 const buttonParentStyle = {
     marginBottom: '28px'
 };
-
 // Temporary button Styles
 const buttonStyles = {
     position: 'relative',
@@ -34,35 +31,40 @@ const buttonStyles = {
     display: 'block',
     marginTop: '3%'
 };
-
 // Active User's Information
-function ActiveUser(props) {
+function ActiveUser() {
     return (
-        <div className="row active-user" style={activeUserStyle}>
-            <div className="col-sm-3" style={activeUserInfoStyle}>
-                {console.log(props.userProfile.name)}
-                <h2>{props.userProfile.name}</h2>
-                <h3>Rank: 1000</h3>
-                <h4>***</h4>
-            </div>
-            <div className="col-sm-6" style={buttonParentStyle}>
-                <Button variant="contained" color="primary" style={buttonStyles}>
-                    Github
-                </Button>
-                <Button variant="contained" color="primary" style={buttonStyles}>
-                    Git Stats
-                </Button>
-                <GitBattleButton />
-                <FormModal />
-            </div>
-            <div className="col-sm-3">
-                <div className="profile-pic-holder">
-                    <img src={props.userProfile.avatarUrl} alt="Profile pic" />
-                </div>
-            </div>
-            <br />
-
-        </div>
+        <AuthContext.Consumer>
+            {(context) => {
+                const { userProfile } = context;
+                return (
+                    <div className="row active-user" style={activeUserStyle}>
+                        <div className="col-sm-3" style={activeUserInfoStyle}>
+                            {console.log(userProfile.name)}
+                            <h2>{userProfile.name}</h2>
+                            <h3>Rank: 1000</h3>
+                            <p>{userProfile.bio}</p>
+                        </div>
+                        <div className="col-sm-6" style={buttonParentStyle}>
+                            <Button variant="contained" color="primary" style={buttonStyles}>
+                                Github
+                            </Button>
+                            <Button variant="contained" color="primary" style={buttonStyles}>
+                                Git Stats
+                            </Button>
+                            <GitBattleButton />
+                            <FormModal />
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="profile-pic-holder">
+                                <img src={userProfile.avatarUrl} alt="Profile pic" />
+                            </div>
+                        </div>
+                        <br />
+                    </div>
+                )
+            }}
+        </AuthContext.Consumer>
     )
 }
 export default ActiveUser;
--- fazeclan/client/src/pages/Home.jsx
+++ fazeclan/client/src/pages/Home.jsx
@@ -4,51 +4,15 @@ import Wrapper from '../components/Layout/Wrapper'
 import ActiveUserDisplay from '../components/Layout/ActiveUserDisplay'
 import TopUser from '../components/Layout/TopUser'
 import UserFavorites from '../components/Layout/UserFavorites'
-import API from "../utils/api"
-import AuthContextProvider, { AuthContext } from '../contexts/AuthContext';
 
 class Home extends Component {
-
-  // load data from graphQL database
-  static contextType = AuthContext;
-  state = {
-    userProfile: {},
-
-  }
-
-  //  gitInfo = () => {
-  //   let value = this.context;
-  //     console.log("this.context: ", value);
-  //     API.getGraphQL(value.activeUser)
-  //       .then(results => {
-  //         console.log("results: ", results.data.data.user);
-  //         this.setState({
-  //           userProfile: results.data.data.user
-  //         })
-  //       })
-  //   }
-
-  componentDidMount() {
-
-    // let value = this.context;
-    // this.gitInfo()
-
-  }
-
-
   render() {
-
-    let value = this.context;
-    console.log("render value: ", this.context);
     return (
       <Wrapper>
         <div className="container">
-          <AuthContext.Consumer>
-            <ActiveUserDisplay userProfile={value.userProfile} />
-          </AuthContext.Consumer>
+          <ActiveUserDisplay />
           <UserFavorites />
           <TopUser />
-
         </div>
       </Wrapper>
     );
