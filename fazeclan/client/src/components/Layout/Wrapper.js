1e769c83003b34b9aa12340765da1beb2f0f0e7c
acarrillo2019 <alric21@aol.com>
2020-05-05 21:13:08 -0700

    bg styling

--- fazeclan/client/src/components/Layout/Wrapper.js
+++ fazeclan/client/src/components/Layout/Wrapper.js
@@ -5,16 +5,25 @@ import BgImage from '../../images/dark_wood.jpg'
 const Logo = require('../../images/gitclub_logo.png');
 
 
-
+// Container Styles
 const containerStyle = {
     padding: '0px',
-    backgroundImage: `url(${BgImage})`
+    backgroundImage: `url(${BgImage})`,
+    backgroundRepeat: 'round',
+    backgroundSize: 'cover'
 }
 
+// Body Styles
+const bodyStyle = {
+  height: '100vh'
+}
+
+// Logo Styles
 const logoStyle = {
     width: '150px'
 }
 
+// Nav Styles
 const navStyle = {
     borderBottom: '3px, white',
     backgroundColor: '303030'
@@ -24,19 +33,21 @@ function Wrapper(props) {
   return (
     <>
       <div className="container-fluid" style={containerStyle}>
-        <div className="navbar gc_nav" style={navStyle}>
-          
-          
-          <a className="title" href="/">
-            <img className="logo" style={logoStyle} src={Logo} alt=""/>
-          </a>
-          <SignInLink />
-          <div>
-            <Search/> 
+        <div className="body" style={bodyStyle}>
+          <div className="navbar gc_nav" style={navStyle}>
+            
+            
+            <a className="title" href="/">
+              <img className="logo" style={logoStyle} src={Logo} alt=""/>
+            </a>
+            <SignInLink />
+            <div>
+              <Search/> 
+            </div>
           </div>
-        </div>
-        
-        <div className={`wrapper ${props.class}`}>{props.children}
+          
+          <div className={`wrapper ${props.class}`}>{props.children}
+          </div>
         </div>
       </div>
       
