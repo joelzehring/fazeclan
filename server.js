bc8dea7a9669aa81bfa5901e8410a654013a80bb
Joel Zehring <joelzehring@gmail.com>
2020-05-09 14:18:31 -0700

    deploy change server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(
   })
 );
 
-app.use('/', express.static(path.join(__dirname, '/client/build')));
+// app.use('/', express.static(path.join(__dirname, '/client/build')));
 
 // set up routes
 app.use("/auth", authRoutes);
