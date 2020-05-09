1b477b4a868e5a015ad0aa8eaff79701e1b87a35
phil cowan <philipjcowan1@gmail.com>
2020-05-09 10:55:04 -0700

    fixed a compile error in battle.jsx

--- client/src/pages/battle.jsx
+++ client/src/pages/battle.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
-<<<<<<< HEAD:client/src/pages/battle.jsx
-import Wrapper from '../components/Wrapper/index'
-=======
 import Wrapper from '../components/Wrapper'
->>>>>>> deb6722785c9f8ec405ce1983cbc32c8cae45a6d:fazeclan/client/src/pages/battle.jsx
 
 class Battle extends Component {
   render() {
