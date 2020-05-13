9a3ebdd7aa39253cdfbf77ac7f7e7030d310b1cc
phil cowan <philipjcowan1@gmail.com>
2020-05-12 19:51:06 -0700

    style stuff

--- client/src/components/PrivateView/index.js
+++ client/src/components/PrivateView/index.js
@@ -5,8 +5,8 @@ import UserFavorites from '../UserFavorites';
 function PrivateView () {
     return (
         <div>
-        <TopUser />
-        <UserFavorites />
+        {/* <TopUser />
+        <UserFavorites /> */}
         </div>
     )
 }
--- client/src/components/PublicView/index.js
+++ client/src/components/PublicView/index.js
@@ -3,18 +3,22 @@ import SignInLink from '../SignInLink'
 import TopUser from '../TopUser'
 
 const backGroundStyle = {
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     width: '100%',
     height: '1000px',
     margin: '0px',
     paddingTop: '15%'
 }
 const titleStyle = {
-    color: 'white',
+    color: 'black',
+    fontSize: '64px'
+}
+const clubStyle = {
+    color: 'rgb(63,81,181)',
     fontSize: '64px'
 }
 const descStyle = {
-    color: 'white',
+    color: 'black',
     fontSize: '32px'
 }
 const signInDivStyle = {
@@ -25,7 +29,8 @@ const signInDivStyle = {
     float: 'right',
     marginRight: '25%',
     backgroundColor: 'white',
-    textAlign: 'center'
+    textAlign: 'center',
+    color: 'black'
 }
 const titleDivStyle = {
     // border: '1px white solid',
@@ -40,14 +45,16 @@ const titleDivStyle = {
 export default function PublicView() {
     return (
         <div style={backGroundStyle}>
+            <div>
             <div style={titleDivStyle}>
-            <h1 style={titleStyle}>GitClub</h1>
+            <h1 style={titleStyle}>Git<span style={clubStyle}>Club</span></h1>
             <h2 style={descStyle}>An app for GitHub users that provides insight and data on their activity in a fun social environment. Created by Alfred Joel Kimberly Phil and Rachel...</h2>
             </div>
-            <div style={signInDivStyle}><h4> Sign in with github to see your stats, earn badges, and see how you stack up to other users. </h4>
+            <div style={signInDivStyle}><h4> Sign in with github</h4>
             <a href="http://localhost:3001/auth/github">
             <img src="/assets/images/githubicon.png" alt="github icon click to sign in"/>
         </a>
+        </div>
         </div>
         </div>
     )
