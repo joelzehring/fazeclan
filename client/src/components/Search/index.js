68ca53296b236ddfb96f8712ba1728a0af4d577c
kimberlycase91 <kimberlycase91@gmail.com>
2020-05-14 17:05:27 -0700

    changed placeholder text and button on search component

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -5,8 +5,8 @@ import React from "react"
 function Search(props) {
   return (
     <form className="form-inline my-2 my-lg-0">
-      <input name="search" className="form-control mr-sm-2" type="search" value={ props.search }  onChange={ props.handleChange }placeholder="Search" aria-label="Search"/>
-      <button onClick={props.handleSubmit} className="btn btn-outline-light my-2 my-sm-0" type="submit">Enter a GitHub username</button>
+      <input name="search" className="form-control mr-sm-2" type="search" value={ props.search }  onChange={ props.handleChange }placeholder="Enter a GitHub username" aria-label="Search"/>
+      <button onClick={props.handleSubmit} className="btn btn-outline-dark my-2 my-sm-0" type="submit">Submit</button>
     </form>
   )
 }
