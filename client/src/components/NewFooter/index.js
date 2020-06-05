29944eb1d9e6166cf3408c23d122dafb600b159f
phil cowan <philipjcowan1@gmail.com>
2020-06-05 16:32:46 -0700

    refactored css for publicview and newfooter to be in css files

--- client/src/components/NewFooter/index.js
+++ client/src/components/NewFooter/index.js
@@ -5,7 +5,7 @@ import './NewFooter.css'
 export default function NewFooter() {
     return (
         <div>
-            <footer className={"footer"}>Created by <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/acarrillo2019');}}>Alfred </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/joelzehring');}}>Joel </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/kimberlycase91');}}>Kimberly </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/phil4lif');}}>Phil  </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/sharkrachel');}}>Rachel</Link></footer>
+            <footer className="footer">Created by <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/acarrillo2019');}}>Alfred </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/joelzehring');}}>Joel </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/kimberlycase91');}}>Kimberly </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/phil4lif');}}>Phil  </Link> | <Link to="route" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/sharkrachel');}}>Rachel</Link></footer>
         </div>
     )
 }
--- client/src/components/PublicView/index.js
+++ client/src/components/PublicView/index.js
@@ -1,52 +1,13 @@
 import React from 'react'
-import SignInLink from '../SignInLink'
-import TopUser from '../TopUser'
-import { Link } from 'react-router-dom';
-import NewFooter from '../NewFooter';
-import AppBar from '@material-ui/core/AppBar';
-
-const backGroundStyle = {
-    backgroundColor: 'white',
-    width: '100%',
-    height: '100vh',
-    margin: '0px',
-    paddingTop: '15%'
-}
-const titleStyle = {
-    color: 'black',
-    fontSize: '64px'
-}
-const clubStyle = {
-    color: 'rgb(63,81,181)',
-    fontSize: '64px'
-}
-const descStyle = {
-    color: 'black',
-    fontSize: '32px'
-}
-const signInStyle = {
-    width: '300px',
-    textAlign: 'center',
-    backgroundColor: 'rgb(63,81,181)'
-}
-const titleDivStyle = {
-    // border: '1px white solid',
-    borderRadius: '3px',
-    width: '60%',
-    height: '400px',
-    // float: 'left',
-    marginLeft: '20%',
-    textAlign: 'left'
-}
-
+import './PublicView.css'
 
 export default function PublicView() {
     return (
-        <div style={backGroundStyle}>
+        <div className='bg'>
             <div>
-            <div style={titleDivStyle}>
-            <h1 style={titleStyle}>Git<span style={clubStyle}>Club</span></h1>
-            <h2 style={descStyle}>An app for GitHub users that provides insight and data on their activity in a fun social environment.</h2>
+            <div className='titlediv'>
+            <h1 className='titlestyle'>Git<span className='clubstyle'>Club</span></h1>
+            <h2 className='descstyle'>An app for GitHub users that provides insight and data on their activity in a fun social environment.</h2>
             <a href={"http://localhost:3001/auth/github"}>
             <img src="/assets/images/githubsignonbutton.png" /></a>
             </div>
